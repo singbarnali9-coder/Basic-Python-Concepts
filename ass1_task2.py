f824356aa2fc5783dbdbfdec20db8ccb2efa2bed
BARNALI SING <singbarnali9@gmail.com>
2025-10-25 13:11:29 +0530

    Add files via upload

---
+++ ass1_task2.py
@@ -0,0 +1,9 @@
+"""taking input from user"""
+first_name=input("Enter the first name: ")
+last_name=input("Enter the last name: ")
+
+"""concatenate both the inputs"""
+full_name=first_name+" "+last_name
+
+"""final output"""
+print("Hello," + " " + full_name + "! Welcome to the Python program")
