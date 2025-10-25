4cf8cb5c745e415e55933ce9b3e35e2bf95d6a2d
singbarnali9-coder <singbarnali@gmail.com>
2025-10-25 17:53:31 +0530

    two python projects which--- 1. performing some basic operations 2. display a greet message

---
+++ ass1_task1.py
@@ -0,0 +1,14 @@
+"""taking input from user"""
+first_num = int(input("Enter the first number: "))
+second_num = int(input("Enter the second number: "))
+
+
+"""performing and displaying operations"""
+Addition=first_num+second_num
+print("Addition: ", Addition)
+Subtraction=first_num-second_num
+print("Subtraction: ", Subtraction)
+Multiplication=first_num*second_num
+print("Multiplication: ", Multiplication)
+Division=float(first_num/second_num)
+print("Division: ", Division)
